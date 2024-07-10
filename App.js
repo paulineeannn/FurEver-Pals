b408a534b6320932783a2c445b3aa6bb1c647faf
Pauline Ann Bautista <paulineannbautista@gmail.com>
2024-07-10 17:54:12 +0800

    removed adopt me button

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ import ViewAdopt from './screens/ViewAdopt';
 import Community from './screens/Community';
 import AddAdopt from './screens/AddAdopt';
 import AdoptionForm from './screens/AdoptionForm';
+import ViewPosted from './screens/ViewPosted';
 import BottomNavigationBar from './screens/BottomNavigationBar';
 
 const Stack = createNativeStackNavigator();
@@ -35,6 +36,7 @@ export default function App() {
         <Stack.Screen name="Community" component={Community} />
         <Stack.Screen name="AddAdopt" component={AddAdopt} />
         <Stack.Screen name="AdoptionForm" component={AdoptionForm} />
+        <Stack.Screen name="ViewPosted" component={ViewPosted} />
         <Stack.Screen name="BottomNavigationBar" component={BottomNavigationBar} />
       </Stack.Navigator>
     </NavigationContainer>
